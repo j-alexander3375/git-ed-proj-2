069ef50ad55fc2a66081c5d40ff25e4b588d9e07
Joshua <joshuaalexander0927@gmail.com>
2024-10-28 19:31:05 +0000

    Add own information to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Joshua Alexander",
+  title: "Software Engineering Student",
+  email: "jalexander.coding9@gmail.com",
+  gitHub: "https://github.com/j-alexander3375",
+  linkedIn: "https://www.linkedin.com/in/joshua-alexander-507694325/",
 };
 
 const primaryColor = "#4E567E";
